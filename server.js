a19cbcb85422fbb09d5277486c2cedb3681ed30c
Gabriel Nascimento <gsnaxsi@gmail.com>
2024-01-24 15:25:25 -0300

    fix server functions

--- server.js
+++ server.js
@@ -9,15 +9,26 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html")
 })
 
-io.on('connection', (socket) => {
-  console.log('Client connected')
-  socket.on('disconnect', () => {
-    console.log('Client disconnected')
-  });
+io.on('connection', async (socket) => {
+    const allUsers = (await io.in("room-1").fetchSockets()).map(u => u.id);
+    console.log(allUsers);
 
-  socket.on('message', (msg) => {
-    io.emit('message', msg)
-  })
+    socket.on("join-room", async (s) => {
+        socket.join(s);
+        const allUsers = (await io.in(s).fetchSockets()).map(u => u.id);
+        console.log(allUsers);
+    });
+
+    socket.on("create-room", async (s) => {
+        const roomCode = (Math.random() + 1).toString(36).substring(7);
+        socket.emit("room-created", roomCode);
+    });
+
+    socket.on("event", async(e) => {
+        socket.rooms.forEach(r => {
+            if(r != socket.id) io.in(r).emit("event", e);
+        });
+    })
 })
 
 
